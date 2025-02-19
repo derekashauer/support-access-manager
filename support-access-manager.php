da90d6b1f21cbd4b9194c6bba966dc3b349b3f57
Derek Ashauer <derek@ashwebstudio.com>
2025-02-18 19:54:29 -0700

    Allow install as plugin

---
+++ support-access-manager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Support Access Manager
+ * Description: Create temporary WordPress admin users with expiration and access limits.
+ * Version: 1.0.0
+ * Author: Derek Ashauer
+ * Author URI: https://www.ashwebstudio.com
+ * License: GPL-3.0+
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Otherwise, load the class manually.
+if ( ! class_exists( 'Support_Access_Manager' ) ) {
+	require_once __DIR__ . '/class-support-access-manager.php';
+}
